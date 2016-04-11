6b53cfb5a6f83db7b13c3ea68adad5fb6981ed32
sveteto13 <shane.veteto@uky.edu>
2016-04-10 22:10:22 -0400

    Update victorian.sql
    The USE srve222 command at the top of the file tells us what database to put the tables into. The commands create two tables, book and comments. These tables are used to store the information about the book as well as the comments that people give about the book.When the user searches for something, the database is accessed and the book table is searched through with the search query being changed depending on what is being search for. The comments table is added to when somebody adds a new comment about a certain book. 
    
    The comments table includes a foreign key into the book table accessing the bid that way the two tables can easily be connected. in order to show the correct comments on a certain books webpage.

--- victorian.sql
+++ victorian.sql
@@ -1,7 +1,7 @@
 USE srve222;
 				
 CREATE TABLE book (
-	bid	BIGINT AUTO_INCREMENT PRIMARY KEY,
+	bid	BIGINT AUTO_INCREMENT PRIMARY KEY, 
 	title	VARCHAR(100) NOT NULL,
 	publish	VARCHAR(300), 
 	setting	VARCHAR(200),
@@ -13,11 +13,11 @@ CREATE TABLE book (
 	author	VARCHAR(100) NOT NULL,
 	url	VARCHAR(200) NOT NULL,
 	UNIQUE KEY(title, author)
-);	
+)		COMMENT= The book table stores all of the information about the book. On the website, when the user uses the search function, the databse will be accessed and it will search through this table as well as a certain column of the table.;	
 						
 CREATE TABLE comments (
 	coid	BIGINT AUTO_INCREMENT PRIMARY KEY,
 	bid	BIGINT NOT NULL,
 	value	VARCHAR(1500) NOT NULL,
 		FOREIGN KEY (bid) REFERENCES book(bid)
-);			
+)		COMMENT= The comment table will hold the comments anyone has about the book. When looking at a books webpage, somebody can hit add comment and a new entry will be stored into this table.;	
